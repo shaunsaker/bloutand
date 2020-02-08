e9c2222243db4f4fe2e7c5d585e210b7478f59f3
Shaun Saker <shaun@aux.co.za>
2020-02-08 20:02:58 +0200

    Remove mui typography theming

--- src/renderer/config/theme/index.ts
+++ src/renderer/config/theme/index.ts
@@ -3,9 +3,6 @@ import { createMuiTheme } from "@material-ui/core/styles";
 import { colors } from "../colors";
 
 export const theme = createMuiTheme({
-  typography: {
-    fontFamily: '"Source Sans Pro", "Helvetica", "Arial", sans-serif'
-  },
   palette: {
     primary: {
       main: colors.primary
