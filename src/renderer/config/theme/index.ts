ccb38471f622d5cbd363b147bf211c0d5588130d
Shaun Saker <shaun@aux.co.za>
2020-02-09 08:15:08 +0200

    Add typography to mui theme

--- src/renderer/config/theme/index.ts
+++ src/renderer/config/theme/index.ts
@@ -3,6 +3,9 @@ import { createMuiTheme } from "@material-ui/core/styles";
 import { colors } from "../colors";
 
 export const theme = createMuiTheme({
+  typography: {
+    fontFamily: '"Source Sans Pro", "Helvetica", "Arial", sans-serif'
+  },
   palette: {
     primary: {
       main: colors.primary
