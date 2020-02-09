d7dc90811542ad8736381a35fce66c8f00fcd61f
Shaun Saker <shaun@aux.co.za>
2020-02-09 19:51:35 +0200

    Update ScanningView styles

--- src/renderer/views/ScanningView/styles.ts
+++ src/renderer/views/ScanningView/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { rhythm } from "../../config";
 
 export const ContentContainer = styled.div`
-  margin-top: ${rhythm.vt}px;
+  margin-top: ${rhythm.vt * 2}px;
 `;
 
 export const DeviceItemContainer = styled.div`
