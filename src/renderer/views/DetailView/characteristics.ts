a9e08938d166d02a5972924d1ea7cadf144f3c55
Shaun Saker <shaun@aux.co.za>
2020-02-11 08:24:31 +0200

    Add a few services and characteristics

---
+++ src/renderer/views/DetailView/characteristics.ts
@@ -0,0 +1,13 @@
+import { Characteristic } from "../../types";
+
+/*
+ * TODO: Complete this list
+ */
+const characteristics: Characteristic[] = [
+  {
+    characteristicName: "Battery Level",
+    characteristicUuid: "0x2A19"
+  }
+];
+
+export default characteristics;
---
+++ src/renderer/views/DetailView/services.ts
@@ -0,0 +1,13 @@
+import { Service } from "../../types";
+
+/*
+ * TODO: Complete this list
+ */
+const services: Service[] = [
+  {
+    serviceName: "Battery Service",
+    serviceUuid: "0x180F"
+  }
+];
+
+export default services;
