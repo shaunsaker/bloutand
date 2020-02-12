ccad524fe93c2833016b809760a8c540a21abe6f
Shaun Saker <shaun@aux.co.za>
2020-02-12 07:31:21 +0200

    Open dev tools in dev only

--- src/main/index.ts
+++ src/main/index.ts
@@ -63,8 +63,10 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // Open the DevTools in development only.
+  if (!app.isPackaged) {
+    mainWindow.webContents.openDevTools();
+  }
 
   // Emitted when the window is closed.
   mainWindow.on("closed", () => {
