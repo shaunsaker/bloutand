2f8d86d90959d056a10b9bad2aae0e438b84ba6e
Shaun Saker <shaun@aux.co.za>
2020-02-12 08:39:16 +0200

    Add fonts

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -3,6 +3,20 @@ import { render } from "react-dom";
 
 import App from "./App";
 
+/*
+ * Add Google Web Fonts as link in head
+ */
+const linkElement = document.createElement("link");
+linkElement.setAttribute("rel", "stylesheet");
+linkElement.setAttribute(
+  "href",
+  "https://fonts.googleapis.com/css?family=Muli:400,800|Source+Sans+Pro:400,600&display=swap"
+);
+document.head.appendChild(linkElement);
+
+/*
+ * Create the root div
+ */
 const mainElement = document.createElement("div");
 document.body.appendChild(mainElement);
 
